beaa4984d35fb418ef57ba45f891c0097db78cfe
taguet <56067630+taguet@users.noreply.github.com>
2023-06-15 14:16:26 +0200

    Added definition for plane plane intersection function

--- inc/GeomEq.hpp
+++ inc/GeomEq.hpp
@@ -7,7 +7,7 @@ namespace Equation {
 	using Vector3f = Eigen::Vector3f;
 	using Vector4f = Eigen::Vector4f;
 
-	/// @brief Represents the parametric equation of a line
+	/// @brief Represents the parametric equation of a line.
 	class Line {
 	public:
 		Line(Vector3f position, Vector3f direction) : position{ position }, direction{ direction } {}
@@ -16,11 +16,16 @@ namespace Equation {
 		Vector3f direction;	/// The direction vector
 	};
 
-	/// @brief Represents the cartesian equation of a plane
+	/// @brief Represents the cartesian equation of a plane.
 	class Plane {
 	public:
 		Plane(Vector3f parameters) : parameters{parameters} {}
 		Plane(float a, float b, float c, float d) : parameters{a,b,c,d} {}
+
+		/// @brief Computes the intersection between this plane and another given plane.
+		/// @param plane The plane intersecting this object.
+		/// @return A line.
+		Line findPlanePlaneintersection(const Plane& plane);
 
 		Vector4f parameters;	/// The parameters a, b, c and d
 	};
